b8767671c8de119dfa00b12e698c6efcfcf7ca52
Raphael <dutrarg1@gmai..com>
2021-08-19 18:23:18 -0300

    add import csv, work

--- app/controllers/api/v1/loads_controller.rb
+++ app/controllers/api/v1/loads_controller.rb
@@ -2,40 +2,40 @@ class Api::V1::LoadsController < Api::V1::ApiController
     before_action :set_load, except: [:index, :create]
     skip_before_action :verify_authenticity_token
     def index
-        @loads = Load.all
-        render json: @loads
+      @loads = Load.all
+      render json: @loads
     end
 
     def show
-        load = Load.find(params[:id])
-        render json: load, status: :ok
+      load = Load.find(params[:id])
+      render json: load, status: :ok
     end
 
     def create
-        @load = Load.new(load_params)
+          @load = Load.new(load_params)
 
         if @load.save
-            render json: @load, status: :created
+          render json: @load, status: :created
         else
-            render json: { errors: @load.errors.full_messages }, status: :unprocessable_entity
+          render json: { errors: @load.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
     def update
         load = Load.find(params[:id])
         if load.update!(load_params)
-            render json: load, status: :ok
+          render json: load, status: :ok
         else
-            render json: load.errors, status: :unprocessable_entity
+          render json: load.errors, status: :unprocessable_entity
         end
     end
 
     def destroy
         load = Load.find(params[:id])
         if load.destroy
-        head :ok
+            head :ok
         else
-        render json: load.errors, status: :unprocessable_entity
+            render json: load.errors, status: :unprocessable_entity
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api do
     namespace :v1 do
-
       resources :loads do
         resources :orders, only: [:create, :index]
       end
-      
       resources :products
-
       resources :orders, only: [:show, :update, :destroy] do
         resources :order_products, except: [:show]
         resources :sorted_order_products, only: [:index]
-      end 
-
+      end
       get '/orders/:id/sorted', to: 'orders#show_sorted'
-
+      post '/imports/csv', to: 'imports#csv'
     end
   end
 end
